c5bd9ca415e5629c103fb9c2e0ca0943731805fd
LiluRi <lilenriquelme@gmail.com>
2025-01-13 19:50:10 -0300

    first commit

---
+++ colabora.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/styles.css"> 
+        <link rel="stylesheet" href="css/colabora.css"> 
+        <script src="https://kit.fontawesome.com/8d48c4a151.js" crossorigin="anonymous"></script> <!--FONTAWESOME -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Imperial+Script&display=swap" rel="stylesheet"> <!--Fuente nombre -->
+        <link href="https://fonts.googleapis.com/css2?family=Imperial+Script&family=Roboto:wght@500&display=swap" rel="stylesheet">
+        <title>Jorge Cristian Perez</title>
+    </head>
+
+    <?php require_once "superior.php"?>
+
+<body>
+    <div class="titulo">
+        <h1><img src="img/colabora.gif" class="giff">Colaborá</h1>
+    </div>
+    <div class="cuadrado">
+        <div class="datos">
+            <p>Querés colaborar con esta causa?</p>
+            <p>Sumate y sé parte!!</p>
+            <p>Nombre y Apellido</p>
+            <p>CVU: 000000000000000000</p>
+            <p>Alias: xxxxxxxxx</p>
+            <p>CUIT/CUIL: 11111111</p>
+            <p>Banco xxxxx</p>
+        </div>
+    </div>
+</body>
+
+
+<script type='' src=''></script>
+
+</html>
---
+++ superior.php
@@ -0,0 +1,17 @@
+<body>
+<header id="header">
+		<a href="index.php" class="logo"><img class="logo-img" src="img/logo.jpg" alt="Jorge Cristian Perez">Jorge Cristian Perez</a>
+    <div class="respmenu">
+        <input type="checkbox">
+        <i class="fas fa-bars"></i>
+        <i class="fas fa-times"></i>
+            <nav id="menu-hamburguesa">
+                <ul>
+                    <li><a href="index.php">Biografía</a></li>
+                    <li><a href="testimonios.php">Testimonios</a></li>
+                    <li><a href="gracias.php">Gracias</a></li>
+                    <li><a href="colabora.php">Colaborá acá! </a></li>
+                </ul>
+            </nav>
+    </div>
+	</header>

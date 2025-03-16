0c338ec0826230b8aef4257c2b59720ccc61eafe
LiluRi <lilenriquelme@gmail.com>
2025-03-16 00:36:46 -0300

    Estilos index

--- index.php
+++ index.php
@@ -19,69 +19,72 @@
     <h1 class="bio"><img src="img/biografia.gif" class="giff">Biografía</h1>
     </div>
 
-    <div class="txiz">
-        <img src="img/1.jpg" class="imgbio im1">
-        <h1>Sus primeros años</h1>
-        <p>Jorge Cristian Pérez, nació el 28 de septiembre de 1977 en CABA, Argentina. Hijo de Roberto Horacio Pérez y Mercedes Celeste Ferreyra, que se casaron en la parroquia Ntra. Sra. De Luján de Sarandí el 4 de mayo de 1961. Tenía dos hermanos varones mayores que él (Roberto y Gabriel) y una hermana menor (Rocío). Creció con su familia en la ciudad de Wilde. Fue bautizado a los tres meses de vida en la parroquia San José de Villa Domínico el 7 de enero del 1978.</p>
+    <div class="container">
+        <div class="txiz">
+            <img src="img/1.jpg" class="imgbio im1">
+            <h1>Sus primeros años</h1>
+            <p>Jorge Cristian Pérez, nació el 28 de septiembre de 1977 en CABA, Argentina. Hijo de Roberto Horacio Pérez y Mercedes Celeste Ferreyra, que se casaron en la parroquia Ntra. Sra. De Luján de Sarandí el 4 de mayo de 1961. Tenía dos hermanos varones mayores que él (Roberto y Gabriel) y una hermana menor (Rocío). Creció con su familia en la ciudad de Wilde. Fue bautizado a los tres meses de vida en la parroquia San José de Villa Domínico el 7 de enero del 1978.</p>
+        </div>
+        <div class="txdr">
+            <img src="img/2.jpg" class="imgbio im2">
+            <h1>Sus hobbies</h1>
+            <p>Al terminar el secundario decidió estudiar el profesorado de geografía, en el Instituto Superior Dr. Joaquin V. Gonzalez. Comentaba que los mapas en realidad no reflejaban la forma real de los continentes. Que como la tierra era redonda y los mapas planos, los países más cercanos a los polos están dibujados como más anchos y otras tantas cosas que a él le fascinaban. Tenía un modo apasionado de explicar las cosas que más le gustaban.
+            Era hincha del Club Atlético Independiente. Tenía facilidad para practicar deportes (futbol, tenis y mucha bicicleta). Le encantaba la música, especialmente el Rock.
+            </p><img>
+        </div>
+        <div class="txiz">
+            <img src="img/3.jpg" class="imgbio im3">
+            <h1>El comienzo de su historia</h1>
+            <p>Su papá falleció cuando él tenía 12 años. 
+            Aproximadamente a los 16 años, se acercó a la parroquia Lujan de Sarandí, invitado por sus amigos y compañeros del colegio. En ese tiempo se fue integrando con los jóvenes de la Acción Católica. 
+            Pasó por los grupos de pre juveniles y juveniles, participando activamente de vía Crucis vivientes, que solía organizar la parroquia todos los años para Semana Santa. Una vez hizo de “buen ladrón”.
+            En ese mismo tiempo integró una banda de rock y blues en la que tocaba el bajo con algunos jóvenes de la parroquia.  En el año 1994 ganaron los Torneos Juveniles Bonaerenses con una canción escrita por él. Y viajaron a Barcelona representando a Buenos Aires en esa competencia. 
+            En el año 1996, cambio de parroquia, llegando a la Iglesia Santa Catalina de Siena de Dock Sud impulsado por una actitud generosa, dado que en su parroquia anterior todo estaba en orden y la institución funcionaba a la perfección y en esta, en cambio, recién se estaban conformando los grupos de la Acción Católica. 
+            Se integra a ellos y empieza un crecimiento interior muy notable. Descubre las riquezas del Acompañamiento Espiritual con un sacerdote y profundiza un camino de amistad con Jesús que le abre nuevos horizontes en la oración y el apostolado. Empieza a frecuentar el sacramento de la Penitencia, toma su Primera Comunión y recibe el sacramento de la Confirmación ese mismo año. Comienza a asomar en Jorge su capacidad de liderazgo.
+            </p><img>
+        </div>
+        <div class="txdr">
+            <img src="img/4.jpg" class="imgbio im4">
+            <h1>Su relación con los demás y con Dios</h1>
+            <p>Con su animación y cuidado acompañaba a los jóvenes de su edad o más pequeños no solo desde el lugar de delegado sino desde una profunda amistad y hermandad para con ellos. Jorge para muchos era como un hermano mayor y aquel que les mostró el camino hacia Jesús. 
+            En su familia era el “bueno”, buscaba siempre el equilibrio de las cosas, era hijo, hermano o amigo según lo que el otro necesitara y siempre involucrando a Jesús en todo. Gozaba de una opinión más que favorable de parte de la comunidad, ya que era un joven maduro para su edad. 
+            Él era muy feliz sintiéndose cerca de Dios, practicando la fe. Sentía alegría en el dar y en servir a los demás. Vivía en lo sencillo y creía en lo milagroso de la oración. No esperaba que los jóvenes se acercaran a la parroquia. Los iba a buscar y usaba el fútbol como excusa para atraerlos.   
+            Participaba no sólo de la misa dominical, sino que cuando podía iba entre semana a escuchar la Palabra y recibir la Sagrada Comunión. Amaba rezar y cantar junto al Sagrario e invitaba a otros jóvenes para que lo acompañaran. 
+            </p><img>
+        </div>
+        <div class="txiz">
+            <img src="img/5.jpg" class="imgbio im5">
+            <h1>Vocación</h1>
+            <p>En la segunda parte del año 1997, hizo una experiencia vocacional de tres días, en el seminario Mayor de Villa Mercedes, Provincia de Buenos Aires.  Esa experiencia le ayudó a afirmar más su vocación como joven cristiano laico en la parroquia; con sus amigos, la familia y el estudio.  </p><img>
+        </div>
+        <div class="txdr">
+            <img src="img/6.jpg" class="imgbio im6">
+            <h1>Misionero</h1>
+            <p>En enero de 1998 fue parte del grupo misionero que existía en la parroquia. En ese verano fueron al paraje Los Rosales, del departamento de Anta, provincia de Salta, dónde ayudó y se dejó ayudar por las buenas personas de ese lugar. 
+            En 1999 compartió con la comunidad de la parroquia Ntra. Sra. Del Rosario, en la localidad de 9 de Julio en la provincia de San Juan, una Semana Santa entera, ayudando al párroco de allí y compartiendo experiencias de vida con los jóvenes. Quedó tan vinculado, que meses después, volvió allí, adelantándose unos días a la Asamblea Federal de la Acción Católica Argentina, cuya sede iba a ser en la misma ciudad de San Juan. Se lo recuerda, entre otras cosas, como organizador de una noche heroica ante el Santísimo, con su guitarra, el canto y la oración. En esta provincia se lo recuerda muy bien hasta el día de hoy, particularmente en la localidad de 9 de Julio
+            </p><img>
+        </div>
+        <div class="txiz">
+            <img src="img/7.jpg" class="imgbio im7">
+            <h1>Sus esfuerzos para ir a la JMJ y su relación con los jóvenes </h1>
+            <p>En el año 2000 en el mes de agosto se realizaba la JMJ en la ciudad de Roma: un acontecimiento tan esperado por muchos y especialmente por Jorge, quien pudo inscribirse y pagar algunas cuotas para participar de la misma. El dinero lo obtenía como fruto de su esfuerzo y trabajo. Jorge custodiaba y cuidaba del templo parroquial de Luján de Sarandí, durante las mañanas de los días hábiles de la semana. 
+            Los sábados a la mañana, limpiaba los patios de un colegio privado que estaba a unos metros de la Iglesia (Colegio Santo Tomás). Siempre vinculado con los jóvenes de la parroquia de Santa Catalina, que tanto tiempo y amor puso en ellos. 
+            Se desplazaba en bicicleta y hasta muchas veces hacía largas distancias por amor al deporte acompañado por otros jóvenes. Mientras cuidaba de la Iglesia, aprovechaba el tiempo y ayudaba dándoles apoyo escolar a aquellos muchachos que no llegaban muy bien con sus materias a fin de año. Y por el testimonio de algunos de ellos, los resultados eran bastante buenos. 
+            Tenía tiempo para todos. Para trabajar, estudiar, para su novia, para su familia, para la parroquia. 
+            </p><img>
+        </div>
+        <div class="txdr">
+            <img src="img/8.jpg" class="imgbio im8">
+            <h1>Su fallecimiento </h1>
+            <p>En su último fin de semana de vida, trabajó esa mañana del sábado en el colegio Santo Tomas; participó de las actividades parroquiales con los jóvenes como todos los sábados en Dock Sud; asistió de la misa de 19hs en la Iglesia Jubilar de Luján de Sarandí, que era una de las tres designadas para ganar indulgencias. Indulgencia que evidente buscó alcanzar. 
+            Al día siguiente domingo 5 de marzo, participó de una convivencia con los jóvenes para planificar el año en un recreo de Villa Elisa, la que duró todo el día. 
+            En el anochecer llego a su casa y pudo encontrase con su mamá. Doña Mercedes, trabajaba toda la semana con cama adentro, cuyo horario comenzaba en la noche del domingo. La acompañó a la parada del colectivo para despedirse de ella y es allí, que recibió una bala en su cuerpo que lo hirió mortalmente. Fue una bala perdida que surgió de un hecho confuso a varios metros de allí. 
+            Falleció en el hospital de Wilde unas horas después. Cuando lo trasladaban al hospital un amigo de su hermano le prometió venganza para quienes lo habían lastimado. Jorge expresó “déjalos, yo ya los perdoné”. 
+            En su velatorio hubo muchísima gente. Por supuesto jóvenes venidos de distintos lugares:D  de su barrio; de su parroquia; de otras Iglesias; compañeros del secundario; compañeros del estudio terciario. Tuvo su Misa Exequial, en la parroquia Santa Catalina y cuando pasó por la parroquia de Luján de Sarandí, junto con el sonar de las campanas a duelo, en el colegio Santo Tomas hicieron formar al personal y alumnos sobre la vereda para ver pasar al cortejo fúnebre. Sus restos mortales descansan en el cementerio de Avellaneda.
+            </p><img>
+        </div>
     </div>
-    <div class="txdr">
-        <img src="img/2.jpg" class="imgbio im2">
-        <h1>Sus hobbies</h1>
-        <p>Al terminar el secundario decidió estudiar el profesorado de geografía, en el Instituto Superior Dr. Joaquin V. Gonzalez. Comentaba que los mapas en realidad no reflejaban la forma real de los continentes. Que como la tierra era redonda y los mapas planos, los países más cercanos a los polos están dibujados como más anchos y otras tantas cosas que a él le fascinaban. Tenía un modo apasionado de explicar las cosas que más le gustaban.
-        Era hincha del Club Atlético Independiente. Tenía facilidad para practicar deportes (futbol, tenis y mucha bicicleta). Le encantaba la música, especialmente el Rock.
-        </p><img>
-    </div>
-    <div class="txiz">
-        <img src="img/3.jpg" class="imgbio im3">
-        <h1>El comienzo de su historia</h1>
-        <p>Su papá falleció cuando él tenía 12 años. 
-        Aproximadamente a los 16 años, se acercó a la parroquia Lujan de Sarandí, invitado por sus amigos y compañeros del colegio. En ese tiempo se fue integrando con los jóvenes de la Acción Católica. 
-        Pasó por los grupos de pre juveniles y juveniles, participando activamente de vía Crucis vivientes, que solía organizar la parroquia todos los años para Semana Santa. Una vez hizo de “buen ladrón”.
-        En ese mismo tiempo integró una banda de rock y blues en la que tocaba el bajo con algunos jóvenes de la parroquia.  En el año 1994 ganaron los Torneos Juveniles Bonaerenses con una canción escrita por él. Y viajaron a Barcelona representando a Buenos Aires en esa competencia. 
-        En el año 1996, cambio de parroquia, llegando a la Iglesia Santa Catalina de Siena de Dock Sud impulsado por una actitud generosa, dado que en su parroquia anterior todo estaba en orden y la institución funcionaba a la perfección y en esta, en cambio, recién se estaban conformando los grupos de la Acción Católica. 
-        Se integra a ellos y empieza un crecimiento interior muy notable. Descubre las riquezas del Acompañamiento Espiritual con un sacerdote y profundiza un camino de amistad con Jesús que le abre nuevos horizontes en la oración y el apostolado. Empieza a frecuentar el sacramento de la Penitencia, toma su Primera Comunión y recibe el sacramento de la Confirmación ese mismo año. Comienza a asomar en Jorge su capacidad de liderazgo.
-        </p><img>
-    </div>
-    <div class="txdr">
-        <img src="img/4.jpg" class="imgbio im4">
-        <h1>Su relación con los demás y con Dios</h1>
-        <p>Con su animación y cuidado acompañaba a los jóvenes de su edad o más pequeños no solo desde el lugar de delegado sino desde una profunda amistad y hermandad para con ellos. Jorge para muchos era como un hermano mayor y aquel que les mostró el camino hacia Jesús. 
-        En su familia era el “bueno”, buscaba siempre el equilibrio de las cosas, era hijo, hermano o amigo según lo que el otro necesitara y siempre involucrando a Jesús en todo. Gozaba de una opinión más que favorable de parte de la comunidad, ya que era un joven maduro para su edad. 
-        Él era muy feliz sintiéndose cerca de Dios, practicando la fe. Sentía alegría en el dar y en servir a los demás. Vivía en lo sencillo y creía en lo milagroso de la oración. No esperaba que los jóvenes se acercaran a la parroquia. Los iba a buscar y usaba el fútbol como excusa para atraerlos.   
-        Participaba no sólo de la misa dominical, sino que cuando podía iba entre semana a escuchar la Palabra y recibir la Sagrada Comunión. Amaba rezar y cantar junto al Sagrario e invitaba a otros jóvenes para que lo acompañaran. 
-        </p><img>
-    </div>
-    <div class="txiz">
-        <img src="img/5.jpg" class="imgbio im5">
-        <h1>Vocación</h1>
-        <p>En la segunda parte del año 1997, hizo una experiencia vocacional de tres días, en el seminario Mayor de Villa Mercedes, Provincia de Buenos Aires.  Esa experiencia le ayudó a afirmar más su vocación como joven cristiano laico en la parroquia; con sus amigos, la familia y el estudio.  </p><img>
-    </div>
-    <div class="txdr">
-        <img src="img/6.jpg" class="imgbio im6">
-        <h1>Misionero</h1>
-        <p>En enero de 1998 fue parte del grupo misionero que existía en la parroquia. En ese verano fueron al paraje Los Rosales, del departamento de Anta, provincia de Salta, dónde ayudó y se dejó ayudar por las buenas personas de ese lugar. 
-        En 1999 compartió con la comunidad de la parroquia Ntra. Sra. Del Rosario, en la localidad de 9 de Julio en la provincia de San Juan, una Semana Santa entera, ayudando al párroco de allí y compartiendo experiencias de vida con los jóvenes. Quedó tan vinculado, que meses después, volvió allí, adelantándose unos días a la Asamblea Federal de la Acción Católica Argentina, cuya sede iba a ser en la misma ciudad de San Juan. Se lo recuerda, entre otras cosas, como organizador de una noche heroica ante el Santísimo, con su guitarra, el canto y la oración. En esta provincia se lo recuerda muy bien hasta el día de hoy, particularmente en la localidad de 9 de Julio
-        </p><img>
-    </div>
-    <div class="txiz">
-        <img src="img/7.jpg" class="imgbio im7">
-        <h1>Sus esfuerzos para ir a la JMJ y su relación con los jóvenes </h1>
-        <p>En el año 2000 en el mes de agosto se realizaba la JMJ en la ciudad de Roma: un acontecimiento tan esperado por muchos y especialmente por Jorge, quien pudo inscribirse y pagar algunas cuotas para participar de la misma. El dinero lo obtenía como fruto de su esfuerzo y trabajo. Jorge custodiaba y cuidaba del templo parroquial de Luján de Sarandí, durante las mañanas de los días hábiles de la semana. 
-        Los sábados a la mañana, limpiaba los patios de un colegio privado que estaba a unos metros de la Iglesia (Colegio Santo Tomás). Siempre vinculado con los jóvenes de la parroquia de Santa Catalina, que tanto tiempo y amor puso en ellos. 
-        Se desplazaba en bicicleta y hasta muchas veces hacía largas distancias por amor al deporte acompañado por otros jóvenes. Mientras cuidaba de la Iglesia, aprovechaba el tiempo y ayudaba dándoles apoyo escolar a aquellos muchachos que no llegaban muy bien con sus materias a fin de año. Y por el testimonio de algunos de ellos, los resultados eran bastante buenos. 
-        Tenía tiempo para todos. Para trabajar, estudiar, para su novia, para su familia, para la parroquia. 
-        </p><img>
-    </div>
-    <div class="txdr">
-        <img src="img/8.jpg" class="imgbio im8">
-        <h1>Su fallecimiento </h1>
-        <p>En su último fin de semana de vida, trabajó esa mañana del sábado en el colegio Santo Tomas; participó de las actividades parroquiales con los jóvenes como todos los sábados en Dock Sud; asistió de la misa de 19hs en la Iglesia Jubilar de Luján de Sarandí, que era una de las tres designadas para ganar indulgencias. Indulgencia que evidente buscó alcanzar. 
-        Al día siguiente domingo 5 de marzo, participó de una convivencia con los jóvenes para planificar el año en un recreo de Villa Elisa, la que duró todo el día. 
-        En el anochecer llego a su casa y pudo encontrase con su mamá. Doña Mercedes, trabajaba toda la semana con cama adentro, cuyo horario comenzaba en la noche del domingo. La acompañó a la parada del colectivo para despedirse de ella y es allí, que recibió una bala en su cuerpo que lo hirió mortalmente. Fue una bala perdida que surgió de un hecho confuso a varios metros de allí. 
-        Falleció en el hospital de Wilde unas horas después. Cuando lo trasladaban al hospital un amigo de su hermano le prometió venganza para quienes lo habían lastimado. Jorge expresó “déjalos, yo ya los perdoné”. 
-        En su velatorio hubo muchísima gente. Por supuesto jóvenes venidos de distintos lugares:D  de su barrio; de su parroquia; de otras Iglesias; compañeros del secundario; compañeros del estudio terciario. Tuvo su Misa Exequial, en la parroquia Santa Catalina y cuando pasó por la parroquia de Luján de Sarandí, junto con el sonar de las campanas a duelo, en el colegio Santo Tomas hicieron formar al personal y alumnos sobre la vereda para ver pasar al cortejo fúnebre. Sus restos mortales descansan en el cementerio de Avellaneda.
-        </p><img>
-    </div>
+
 
 
 <?php require_once "inferior.php"?>

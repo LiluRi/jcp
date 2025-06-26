ff484689e38e9d3def01c7927e0b52124c6091ad
LiluRi <lilenriquelme@gmail.com>
2025-06-25 21:57:45 -0300

    redes sociales

--- inferior.php
+++ inferior.php
@@ -1,11 +1,14 @@
 <footer class="wrapper">
   <section class="linkedIn">
-    <h6>Desarrollado por: <a class="footera" href="https://www.linkedin.com/in/lilen-riquelme-b63647242/"><i class="fa-brands fa-linkedin" style="margin-right: 5px;"></i>Lilén Riquelme</a></h6>
+    <h6>Desarrollo: <a class="footera" href="https://www.linkedin.com/in/lilen-riquelme-b63647242/">Lilén Riquelme</a></h6>
   </section>
   <section class="copyright">
     <h6>©Jorge Cristian Perez -Todos los derechos reservados.</h6>
   </section>
   <section class="escudito">
+    
     <img src="img/escudobicolor.png" class="escudo">
+    <a href="https://www.instagram.com/jorge.cristian.perez/"><img src="img/ig.png" class="redes red"></a>
+    <a href="https://www.facebook.com/Jorge.Cri.Perez"><img src="img/fb.png" class="redes"></a>
   </section>
 </footer>
